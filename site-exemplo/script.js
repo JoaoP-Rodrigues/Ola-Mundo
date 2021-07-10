4aa9582a7da9c50687a0220a97bdcbd91361e2d0
JoaoP-Rodrigues <joao.tribo@gmail.com>
2021-07-10 14:14:20 -0300

    Apenas teste

---
+++ site-exemplo/script.js
@@ -0,0 +1,9 @@
+var title = document.querySelector("#title")
+
+title.setAttribute("class", "testando-atributo")
+
+console.log(title)
+
+var btn = document.querySelector("#btn")
+
+btn.setAttribute("disabled", "disabled")
